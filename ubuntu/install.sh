ad467677cd3358977a1dde49ab76ee98daa8e66d
321core <choijunguk@gmail.com>
2015-08-04 03:10:03 +0900

    fixes

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
+sudo apt-get install python-pip -y
+sudo pip install flask --upgrade
+sudo pip install requests --upgrade
 sudo cp -r ../stalk /usr/share/
 sudo cp stalk.conf /etc/init/
 sudo cp stalkd /usr/sbin
-
+sudo cp stalk /usr/sbin
