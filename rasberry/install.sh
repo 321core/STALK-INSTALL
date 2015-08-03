5179c913c9883c6a88bc9e13b6e620be5eadad13
321core <choijunguk@gmail.com>
2015-08-04 04:37:12 +0900

    fixes

--- rasberry/install.sh
+++ rasberry/install.sh
@@ -4,6 +4,7 @@ sudo apt-get install python-pip -y
 sudo pip install flask --upgrade
 sudo pip install requests --upgrade
 sudo cp -r ../stalk /usr/share/
+sudo rm /etc/init.d/stalk
 sudo cp stalk-service /etc/init.d/stalk
 sudo cp stalkd /usr/sbin
 sudo cp stalk /usr/sbin
