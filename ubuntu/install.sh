4a0739b762840bf40557589251e19e21b3f36b66
Jung-uk Choi <choijunguk@gmail.com>
2015-08-04 18:26:33 +0900

    Update install.sh
    update install.sh for Systemd

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,7 +4,16 @@ sudo apt-get install python-pip -y
 sudo pip install flask --upgrade
 sudo pip install requests --upgrade
 sudo cp -r ../stalk /usr/share/
-sudo cp stalk.conf /etc/init/
 sudo cp stalkd /usr/sbin
 sudo cp stalk /usr/sbin
-sudo service stalk start
+
+RET=$(ps -p1 | grep system)
+if [ -n "$RET" ]; 
+then # SYSTEMD
+  sudo cp stalk.service /lib/systemd/system/
+  sudo systemctl enable stalk.service
+  sudo systemctl start stalk.service
+else # UPSTART
+  sudo cp stalk.conf /etc/init/
+  sudo service stalk start
+fi
