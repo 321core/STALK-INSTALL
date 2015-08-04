49feb36e085c452e54edb770f7ec3ae550b47b27
Jung-uk Choi <choijunguk@gmail.com>
2015-08-05 03:24:26 +0900

    Update install.sh

--- cygwin/install.sh
+++ cygwin/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-sudo rm -rf /usr/share/stalk
-sudo cp -r ../stalk /usr/share/
-sudo cp -r ./requests /usr/share/stalk/service/
-sudo cp -r ./flask /usr/share/stalk/service/
-sudo cp -r ./werkzeug /usr/share/stalk/service/
-sudo cp -r ./jinja2 /usr/share/stalk/service/
-sudo cp -r ./markupsafe /usr/share/stalk/service/
-sudo cp ./itsdangerous.py /usr/share/stalk/service/
-sudo rm -rf /usr/share/stalk/stalkd
-sudo cp stalkd /usr/share/stalk/
-sudo rm -rf /usr/bin/stalk
-sudo cp stalk /usr/bin/
+rm -rf /usr/share/stalk
+cp -r ../stalk /usr/share/
+cp -r ./requests /usr/share/stalk/service/
+cp -r ./flask /usr/share/stalk/service/
+cp -r ./werkzeug /usr/share/stalk/service/
+cp -r ./jinja2 /usr/share/stalk/service/
+cp -r ./markupsafe /usr/share/stalk/service/
+cp ./itsdangerous.py /usr/share/stalk/service/
+rm -rf /usr/share/stalk/stalkd
+cp stalkd /usr/share/stalk/
+rm -rf /usr/bin/stalk
+cp stalk /usr/bin/
