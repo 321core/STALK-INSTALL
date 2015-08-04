4db7a0d53831bf37ed54650589825e652a7d9bf6
Jung-uk Choi <choijunguk@gmail.com>
2015-08-04 19:06:41 +0900

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 set -e
+echo "*** Update Modules..."
 sudo apt-get install python-pip -y
 sudo pip install flask --upgrade
 sudo pip install requests --upgrade
-sudo rm -rf /usr/share/stalk || true
+echo "*** Copy executables..."
+sudo rm -rf /usr/share/stalk
 sudo cp -r ../stalk /usr/share/
-sudo rm -rf /usr/sbin/stalkd || true
+sudo rm -rf /usr/sbin/stalkd
 sudo cp stalkd /usr/sbin
-sudo rm -rf /usr/bin/stalk || true
+sudo rm -rf /usr/bin/stalk
 sudo cp stalk /usr/sbin
+echo "*** Detect init system..."
 RET=$(ps -p1 | grep system)
 if [ -n "$RET" ]; then
-  echo "install STALK as Systemd service..."
-  sudo rm -rf /lib/systemd/system/stalk.service || true
-  sudo cp stalk.service /lib/systemd/system/
-  sudo systemctl enable stalk.service
-  sudo systemctl start stalk.service
+	echo "*** install STALK as Systemd service..."
+	sudo rm -rf /lib/systemd/system/stalk.service
+	sudo cp stalk.service /lib/systemd/system/
+	sudo systemctl enable stalk.service
+	sudo systemctl start stalk.service
 else
-  echo "install STALK as Upstart service..."
-  sudo rm -rf /etc/init/stalk.conf || true
-  sudo cp stalk.conf /etc/init/
-  sudo service stalk start
+	echo "*** install STALK as Upstart service..."
+	sudo rm -rf /etc/init/stalk.conf
+	sudo cp stalk.conf /etc/init/
+	sudo service stalk start
 fi
+echo "*** STALL installed."
