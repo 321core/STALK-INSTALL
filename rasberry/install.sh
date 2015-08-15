f3b4ef6bead124b6c18db95995b13ce75c887c4a
Jung-uk Choi <choijunguk@gmail.com>
2015-08-15 11:03:29 +0900

    Update install.sh

--- rasberry/install.sh
+++ rasberry/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+sudo service stalk stop || true
 sudo apt-get install python-pip -y
 sudo pip install flask --upgrade
 sudo pip install requests --upgrade
 sudo cp -r ../stalk /usr/share/
-sudo rm /etc/init.d/stalk
+sudo rm /etc/init.d/stalk || true
 sudo cp stalk-service /etc/init.d/stalk
 sudo cp stalkd /usr/sbin
 sudo cp stalk /usr/sbin
